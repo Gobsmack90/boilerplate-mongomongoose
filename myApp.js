28bc108bed28684685a1b15914970df7bed05a82
Harrison King <authorharrisonking@gmail.com>
2021-02-01 16:55:34 -0700

    fix

--- myApp.js
+++ myApp.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const mongodb = require('mongodb');
-import mongoose from 'mongoose';
+const mongoose = requre('mongoose');
 
 process.env.MONGO_URI='mongodb+srv://Gobsmack:asdf1234@cluster0.zb2i8.mongodb.net/<dbname>?retryWrites=true&w=majority';
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
